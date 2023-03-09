3181cae2d97bf425b4544bb56853228f15452d8b
AdamHenrique67 <adamhenrique67@gmail.com>
2023-03-09 17:00:18 -0300

    add useState and onChangeText to capture input text of participant input name

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,20 +1,35 @@
-import React from 'react'
-import { Text, View, TextInput, TouchableOpacity, FlatList } from 'react-native';
+import React ,{ useState } from 'react'
+import { Text, View, TextInput, TouchableOpacity, FlatList, Alert } from 'react-native';
 import { Participant } from '../../components/Participant';
 
 import { styles } from './styles';
 
 export function Home() {
+  const [participants, setParticipants] = useState<string[]>([])
+  const [participantName, setParticipantName] = useState('')
 
   function handleParticipantAdd() {
-    console.log('Você clicou em add');
+    if(participants.includes(participantName) || participantName === ''){
+      return Alert.alert('Partipante Existe', 'Já existe um participante na lista com esse nome')
+    }
+    setParticipants(prevState => [...prevState, participantName])
+    setParticipantName('')
   }
 
   function handleParticipantRemove(name: string) {
+    Alert.alert('Remover', `Remover o participante ${name}?`, [
+      {
+        text: 'Sim',
+        onPress: () => Alert.alert('Deletado!')
+      },
+      {
+        text: 'Não',
+        style: 'cancel'
+      }
+    ])
+
     console.log(`Você clicou em remover o participante ${name}`);
   }
-
-  const participants = ['Adam', 'Tuzin', 'Mateus', 'Daniel', 'Josiney', 'Luis', 'Thainá', 'Luana', 'Leticia', 'Gabriel', 'Samuel']
 
   return (
     <View style={styles.container}>
@@ -31,6 +46,8 @@ export function Home() {
           style={styles.input}
           placeholder='Nome do participante'
           placeholderTextColor='#6B6B6B'
+          onChangeText={setParticipantName}
+          value={participantName}
         />
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
           <Text style={styles.buttonText}>
