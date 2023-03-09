fd5c239e7febfbf73598e97c72c8656de1151676
AdamHenrique67 <adamhenrique67@gmail.com>
2023-03-09 14:54:02 -0300

    create Home screen

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+import { styles } from './styles';
+
+export function Home() {
+
+  function handleParticipantAdd() {
+    console.log('Você clicou');
+  }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Nome do evento
+      </Text>
+
+      <Text style={styles.eventData}>
+        Quinta, 9 de marça de 2023
+      </Text>
+
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input}
+          placeholder='Nome do participante'
+          placeholderTextColor='#6B6B6B'
+        />
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
